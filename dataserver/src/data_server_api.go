79cf9891feb5450cd8327621bf839968ccf25dee
wkigen <qiji950@163.com>
2018-01-07 21:12:36 +0800

    base

--- dataserver/src/data_server_api.go
+++ dataserver/src/data_server_api.go
@@ -26,26 +26,27 @@ func (self *DataServer) RegisterUser(ctx context.Context, args *RegisterUserArgs
 	qArgs.Accout = accout
 	qReply := QueryUserReply{}
 	err := self.QueryUser(ctx,&qArgs,&qReply)
-
-	if(err == nil && qReply.Id == -1){
-		password := utils.MD5([]byte(args.Password+self.Salt))
-		if(accout != "" && password != ""){
-			stmt, err := self.DataBase.Prepare("INSERT INTO user VALUES (?,?,?,?)")
-			defer stmt.Close()
-			if( err == nil ){
-				res,err := stmt.Exec(0,accout,accout,password)
-				if(err == nil){
-					reply.Status = 0
-					log.Log(log.Debug,"user registration success, %d",res.LastInsertId)
-					return nil
+	if(err == nil ||err.Error() == "sql: no rows in result set"){
+		if( qReply.Id == -1){
+			password := utils.MD5([]byte(args.Password+self.Salt))
+			if(accout != "" && password != ""){
+				stmt, err := self.DataBase.Prepare("INSERT INTO user VALUES (?,?,?,?)")
+				defer stmt.Close()
+				if( err == nil ){
+					res,err := stmt.Exec(0,accout,accout,password)
+					if(err == nil){
+						reply.Status = 0
+						log.Log(log.Debug,"user registration success,(%s) %d",accout,res.LastInsertId)
+						return nil
+					}
 				}
 			}
+		}else{
+			err = nil
+			log.Log(log.Debug,"user registration fail，error :has same accout in db")
 		}
-	}else{
-		err = errors.New( "has same accout in db")
 	}
-	
-	log.Log(log.Debug,"user registration fail sql error ,%s",err)
+	log.Log(log.Debug,"user registration fail， error :%s",err)
 	reply.Status = 1
     return err
 }
@@ -66,11 +67,12 @@ type QueryUserReply struct {
 
 func (self *DataServer) QueryUser(ctx context.Context, args *QueryUserArgs, reply *QueryUserReply) error {
 	reply.Id = -1
-	rows := self.DataBase.QueryRow("SELECT * FROM user WHERE 'account'=?",args.Accout)
+	rows := self.DataBase.QueryRow("SELECT * FROM user WHERE account = ?",args.Accout)
 	err := rows.Scan(&reply.Id, &reply.Accout, &reply.NickName, &reply.Password)
 	log.Log(log.Debug,"%d %s %s %s",reply.Id, reply.Accout, reply.NickName, reply.Password)
 	if(err == nil){
 		return nil
 	}
+	log.Log(log.Warn,"query user (%s) is error,%s",args.Accout,err)
     return err
 }
