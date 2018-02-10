99e96bab9a486993d87abce09453cb97721266d4
wkigen <qiji950@163.com>
2018-02-10 18:01:29 +0800

    base

--- simulator/main.go
+++ simulator/main.go
@@ -18,7 +18,7 @@ type LoginArgs struct{
 type LoginReply struct{
 	Code int
 	Msg string
-	Id			int32
+	Id			int64
 	Accout 		string
 	NickName 	string
 	Token 		string
@@ -34,11 +34,20 @@ type RegisterUserReply struct {
 	Msg string
 }
 
+type ChangNickNameArgs struct {
+	NewNickName string
+}
+
+type ChangNickNameReply struct {
+	Code int
+	Msg string
+}
+
 func Register(index int){
 	cc := &codec.MsgpackCodec{}
 
 	args := &RegisterUserArgs{
-		Account: "woshishui004",
+		Account: "woshishui001",
 		Password:"123456",
 	}
 
@@ -76,9 +85,11 @@ func Register(index int){
 		log.Fatal("failed to decode reply: ", err)
 	}
 
-	log.Printf("%s , %s ,%s", args.Account, args.Password, reply.Code,reply.Msg)
+	log.Printf("%s , %s ,%d,%s", args.Account, args.Password, reply.Code,reply.Msg)
 }
 
+var userId int64
+var token string
 func Login(index int){
 
 	cc := &codec.MsgpackCodec{}
@@ -122,16 +133,113 @@ func Login(index int){
 		log.Fatal("failed to decode reply: ", err)
 	}
 
-	log.Printf("%s , %s ,%s,%s", args.Account, args.Password, reply.Token,reply.Msg)
+	userId = reply.Id
+	token = reply.Token
+
+	log.Printf(" login %s , %s ,%s,%s", args.Account, args.Password, reply.Token,reply.Msg)
+
+	ChangNickName(reply.Id,reply.Token)
+}
+
+func ChangNickName(userId int64,token string){
+	cc := &codec.MsgpackCodec{}
+
+	args := &ChangNickNameArgs{
+		NewNickName:"fff",
+	}
+
+	data, _ := cc.Encode(args)
+	b := bytes.NewReader(data)
+	req, err := http.NewRequest("POST", "http://127.0.0.1:8701/",b )
+	if err != nil {
+		log.Fatal("failed to create request: ", err)
+		return
+	}
+
+	var mId = 11000
+	h := req.Header
+	h.Set(gateway.XMessageID,strconv.Itoa(mId))
+	h.Set(gateway.XMessageType, "0")
+	h.Set(gateway.XSerializeType, "3")
+	h.Set(gateway.XServicePath, "LoginServer")
+	h.Set(gateway.XServiceMethod, "ChangNickName")
+	h.Set("UserId", strconv.FormatInt(userId,10))
+	h.Set("Token", token)
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal("failed to call: ", err)
+	}
+	defer res.Body.Close()
+
+	replyData, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal("failed to read response: ", err)
+	}
+
+	reply := &ChangNickNameReply{}
+	err = cc.Decode(replyData, reply)
+	if err != nil {
+		log.Fatal("failed to decode reply: ", err)
+	}
+
+	log.Printf(" ChangNickName %d , %s ", reply.Code,reply.Msg)
+
+}
+
+func TestDataServer(index int){
+	cc := &codec.MsgpackCodec{}
+
+	args := &LoginArgs{
+		Account: "1",
+		Password:"1",
+	}
+
+	data, _ := cc.Encode(args)
+	b := bytes.NewReader(data)
+	req, err := http.NewRequest("POST", "http://127.0.0.1:8701/",b )
+	if err != nil {
+		log.Fatal("failed to create request: ", err)
+		return
+	}
+
+	var mId = 10000+index
+	h := req.Header
+	h.Set(gateway.XMessageID,strconv.Itoa(mId))
+	h.Set(gateway.XMessageType, "0")
+	h.Set(gateway.XSerializeType, "3")
+	h.Set(gateway.XServicePath, "DataServer")
+	h.Set(gateway.XServiceMethod, "Login")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal("failed to call: ", err)
+	}
+	defer res.Body.Close()
+
+	replyData, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal("failed to read response: ", err)
+	}
+
+	reply := &LoginReply{}
+	err = cc.Decode(replyData, reply)
+	if err != nil {
+		log.Fatal("failed to decode reply: ", err)
+	}
+
+	log.Printf(" TestDataServer %d , %s ", reply.Code,reply.Msg)
+
 }
 
 func main() {
 	
 	ch := make(chan int) 
-	for index := 0; index <= 2000; index++ {
+	for index := 0; index < 1; index++ {
 		log.Printf("%d",index)
 		go Login(index)
-		go Register(index)
+		//go Register(index)
+		//go TestDataServer(index)
 	}
 	<-ch
 }
